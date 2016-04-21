7c28f597ff9987b47ddbf6811e887e7763fb9f92
Michaël Marinetti <github@marinetti.fr>
2016-04-21 14:14:57 +0200

    set default path to css/
    This prevent the default configuration with "clear_first" to delete all
    php files (or any others)

--- config/less.php
+++ config/less.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 return [
-	'path'        => '',     // relative path to a writable folder to store compiled / compressed css
+	'path'        => 'css/', // destination path (relative to DOCUMENT_ROOT) to store compiled / compressed css
 	'clear_first' => false,  // Clear the provided folder before writing new file
 	'combine'     => false,  // combine multiple files into one
 	'compress'    => false,  // compress even more (after less.php output)
