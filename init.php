cbea071ddcbd33d2eb285df65f88d006c2d4458d
Michaël Marinetti <github@marinetti.fr>
2016-04-21 12:03:21 +0200

    do not use the drop in replacement for lessphp

--- init.php
+++ init.php
@@ -1,3 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-require_once 'vendor/less.php/lessc.inc.php';
+
+if ( !class_exists( 'Less_Parser' ) ) {
+	require_once __DIR__ . '/vendor/less.php/lib/Less/Autoloader.php';
+	Less_Autoloader::register();
+}
